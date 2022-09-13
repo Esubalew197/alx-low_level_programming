62ed26373b5080b78a04f473faaab545663bd9a4
Esubalew197 <esubalewmillion@gmail.com>
2022-09-14 00:28:52 +0300

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * jack_bauer - prints time
- *   *
- *    * Description: prints a list of time
- *     * Return: Always 0.
- *      */
+ ** jack_bauer - prints time
+ **
+ ** Description: prints a list of time
+ ** Return: Always 0.
+ **/
 void jack_bauer(void)
 {
 	int i, j;
