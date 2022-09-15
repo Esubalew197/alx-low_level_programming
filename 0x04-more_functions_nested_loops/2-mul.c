b6a4e43c4c35371b0e215dd7aa3887897c40893d
Esubalew197 <esubalewmillion@gmail.com>
2022-09-15 22:36:04 +0300

    2-mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,13 @@
 
 /**
  ** mul - multiplier function
- ** @m: int to multiply
- ** @n: int to multiply
+ ** m: int to multiply
+ ** n: int to multiply
+ ** a: int to multiply
+ ** b: int to multiply
  ** Return: result of the two multiplied values
  **/
-int mul(int a, int b)
+int mul(int a, int b) /* a: int to multiply, b: int to multiply */
 {
 	return (a * b);
 }
