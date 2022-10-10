42412c6fa7a735c6724c3331f964b7e7776bd58b
Esubalew197 <esubalewmillion@gmail.com>
2022-10-10 21:48:15 +0300

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic information
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: This struct is for a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
