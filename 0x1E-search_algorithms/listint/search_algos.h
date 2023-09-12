104ae485cb9bb9a01b1e4f9ceb6380aa2e07b526
Esubalew197 <esubalewmillion@gmail.com>
2023-09-12 18:34:34 +0300

    advanced tasks

---
+++ 0x1E-search_algorithms/listint/search_algos.h
@@ -0,0 +1,15 @@
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
